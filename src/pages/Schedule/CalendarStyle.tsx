6337d8d5408c44363d21cc2a833345bbedf5a2e6
Eunyeong Jang <jangeunyeong081@gmail.com>
2024-08-30 10:46:00 +0900

    feat 주간 일정 UI 추가

--- src/pages/Schedule/CalendarStyle.tsx
+++ src/pages/Schedule/CalendarStyle.tsx
@@ -3,14 +3,56 @@ import styled from '@emotion/styled';
 import Calendar from 'react-calendar';
 
 export const StyledCalendar = styled(Calendar)({
-  width: '100%',
+  position: 'fixed',
+  width: '330px',
   fontWeight: 'bold',
   backgroundColor: '#fff5f6',
   border: 'none',
+  marginTop: '85px',
+  zIndex: -1,
 
   '& .react-calendar__navigation': {
-    height: '60px',
+    height: '40px',
+  },
 
-    ':hover': {},
+  '& .react-calendar__navigation button': {
+    fontSize: '14px',
+    fontWeight: '400',
+
+    color: 'white',
+    backgroundColor: '#eb6577',
+    ':hover': {
+      backgroundColor: '#eb596d',
+    },
+  },
+
+  '& .react-calendar__month-view__weekdays': {
+    fontSize: '11px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'orange',
+    height: '20px',
+  },
+
+  '& .react-calendar__year-view .react-calendar__tile, .react-calendar__decade-view .react-calendar__tile, .react-calendar__century-view .react-calendar__tile':
+    {
+      height: '40px',
+      color: 'orange',
+      textAlign: 'center',
+    },
+
+  '& .react-calendar__tile': {
+    fontSize: '12px',
+    fontWeight: '600',
+    height: '40px',
+    borderRadius: '10px',
+  },
+  '& .react-calendar__tile--active': {
+    backgroundColor: '#eb6577',
+    borderRadius: '10px',
+
+    ':hover': {
+      backgroundColor: '#e5384f',
+    },
   },
 });
--- src/pages/Schedule/Schedule.tsx
+++ src/pages/Schedule/Schedule.tsx
@@ -1,6 +1,8 @@
-import { Box } from '@mui/material';
 import React from 'react';
+import { Box, Button, Stack, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import { StyledCalendar } from './CalendarStyle';
+import WeeklySchedule from './WeeklySchedule';
 
 const Schedule = () => {
   const [today, setToday] = React.useState(new Date());
@@ -10,8 +12,22 @@ const Schedule = () => {
   };
 
   return (
-    <Box marginTop="150px">
+    <Box marginTop="70px" marginLeft="10px" display="flex">
+      <Stack display="flex" flexDirection="column">
+        <Button
+          variant="outlined"
+          size="large"
+          sx={{
+            boxShadow: '2px 2px 5px 1px rgba(0,0,0,0.25)',
+            marginTop: '20px',
+          }}
+        >
+          <AddIcon sx={{ color: '#e5384f', marginRight: '10px' }} />
+          <Typography color="black">일정 추가</Typography>
+        </Button>
+      </Stack>
       <StyledCalendar onChange={onChangeToday} value={today} />
+      <WeeklySchedule />
     </Box>
   );
 };
